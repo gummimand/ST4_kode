de56c18ff4a8443b08fcabc9a63e6afdd0aa42fd
Nicolaj <nthost15@student.aau.dk>
2019-04-30 14:07:05 +0200

    Matlab data plotter fil

--- Matlab/pilot_dataplot.m
+++ Matlab/pilot_dataplot.m
@@ -1,28 +1,49 @@
 clear all;
 close all;
 %% Import data:
-filename = 'data.txt';
+filename = 'data_gyroTest2.txt';
 delimiterIn = ',';
 headerlinesIn = 1;
 A = importdata(filename,delimiterIn,headerlinesIn);
 data = A.data;
+Fs = 50;
+N = (size(data,2)/6);
+t = (0:N-1)/Fs;
 
 %% Actual processing and plotting of data
-x=zeros(1,size(data,2)/3);
-y=zeros(1,size(data,2)/3);
-z=zeros(1,size(data,2)/3);
+xacc=zeros(1,size(data,2)/6);
+yacc=zeros(1,size(data,2)/6);
+zacc=zeros(1,size(data,2)/6);
 
-for i=1:3:(size(data,2))
-    z = [z(2:end) data(i)];
-    y = [y(2:end) data(i+1)];
-    x = [x(2:end) data(i+2)];    
+xgyr=zeros(1,size(data,2)/6);
+ygyr=zeros(1,size(data,2)/6);
+zgyr=zeros(1,size(data,2)/6);
+
+
+for i=1:6:(size(data,2))
+    zacc = [zacc(2:end) data(i)];
+    yacc = [yacc(2:end) data(i+1)];
+    xacc = [xacc(2:end) data(i+2)];    
+    
+    zgyr = [zgyr(2:end) data(i+3)];
+    ygyr = [ygyr(2:end) data(i+4)];
+    xgyr = [xgyr(2:end) data(i+5)];
 end
 
-plot(x)
+plot(t,xacc)
 hold on
-plot(y)
-plot(z)
-title("Plot of data");
+plot(t,yacc)
+plot(t,zacc)
+title("Plot of accelerometer data");
+legend('x','y','z')
+
+figure;
+
+plot(t,xgyr)
+hold on
+plot(t,ygyr)
+plot(t,zgyr)
+title("Plot of gyroskop data");
 legend('x','y','z')
 
 %%
